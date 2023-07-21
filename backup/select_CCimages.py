45860118682602e0115ea7bb2bc190a58cb822e3
Mimmo <doms-17@hotmail.it>
2023-07-21 09:13:10 +0200

    Update select_CCimages.py

--- backup/select_CCimages.py
+++ backup/select_CCimages.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 
 
 def main():
-    # root_dataset_Or: str = "D:/DOMI/University/Thesis/Coding/Dataset/TestSet/ISIC_not1024"
-    # root_dataset_CC: str = "D:/DOMI/University/Thesis/Coding/Dataset/GANNED_orsize_selected/"
-    # root_dataset_dest: str = "D:/DOMI/University/Thesis/Coding/Dataset/TestSet/CC_ISIC_not1024/"
+    # root_dataset_Or: str = ""
+    # root_dataset_CC: str = ""
+    # root_dataset_dest: str = ""
 
     # imagesOr_filenames = os.listdir(root_dataset_Or)
 
@@ -19,9 +19,9 @@ def main():
     #         if imageOr_filename in imagesCC_filenames:
     #             shutil.copyfile(os.path.join(root_dataset_CC,label,imageOr_filename), os.path.join(root_dataset_dest,imageOr_filename))
 
-    root_dataset_Or: str = "D:/DOMI/University/Thesis/Coding/Dataset/Inference/derma_v0/ISIC_not1024"
-    root_dataset_CC: str = "D:/DOMI/University/Thesis/Coding/Dataset/Inference/derma_v0/CC_ISIC_not1024"
-    root_dataset_dest: str = "D:/DOMI/University/Thesis/Coding/Dataset/Inference/derma_v0/ISIC_not1024_new"
+    root_dataset_Or: str = ""
+    root_dataset_CC: str = ""
+    root_dataset_dest: str = ""
 
     imagesOr_filenames = os.listdir(root_dataset_Or)
     imagesCC_filenames = os.listdir(os.path.join(root_dataset_CC))
